02fccd7915167f126418e0e76fae27a206cfb439
donchucky21 <chucky@gmail.com>
2026-02-08 18:49:24 +0000

    Finalize Bedrock capstone - EKS, IAM RBAC, S3 backend, strict dev access

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "bedrock-tfstate-chukwuka-1021"
-    key     = "bedrock/terraform.tfstate"
+    key     = "project-bedrock/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
   }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,30 @@
 output "region" {
   value = var.aws_region
 }
+
+output "assets_bucket_name" {
+  value = aws_s3_bucket.assets.bucket
+}
+
+output "vpc_id" {
+  value = aws_vpc.bedrock.id
+}
+
+output "cluster_name" {
+  value = aws_eks_cluster.bedrock.name
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.bedrock.endpoint
+}
+
+# Grading: show dev IAM access key (sensitive)
+output "bedrock_dev_view_access_key_id" {
+  value     = aws_iam_access_key.dev_view_key.id
+  sensitive = true
+}
+
+output "bedrock_dev_view_secret_access_key" {
+  value     = aws_iam_access_key.dev_view_key.secret
+  sensitive = true
+}
